2171522f431754901395c6007558d9f6a0e65d84
RITIK AGARWAL <aritik755@gmail.com>
2026-08-29 19:22:17 +0530

    Time: 0 ms (100%), Space: 7.7 MB (83.81%) - LeetHub

---
+++ 2427-number-of-common-factors/2427-number-of-common-factors.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int commonFactors(int a, int b) {
+        int count = 0;
+        int num = 0;
+        if(a > b) num = a;
+        else num = b;
+        for(int i = 1; i <= num; i++){
+            if(a % i == 0 && b % i == 0) count++;
+        }
+        return count;
+    }
+};
